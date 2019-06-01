fc8f4150957c4b3d5ca64831f39fbf550e588eae
Mahatthana Nomsawadi <mahatthana.n@gmail.com>
2019-06-01 14:30:39 +0700

    :recycle: Use Django template

--- hit_me_please/hitters/views.py
+++ hit_me_please/hitters/views.py
@@ -1,16 +1,7 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 from django.views import View
 
 
 class LandingPageView(View):
     def get(self, request):
-
-        html = '''
-            <form action="" method="post">
-                <label for="email">Email:</label>
-                <input id="email" name="email" type="email" />
-                <button type="submit">Submit</button>
-            </form>
-        '''
-
-        return HttpResponse(html)
+        return render(request, template_name='index.html')
