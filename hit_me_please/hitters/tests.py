dfd443a878bf547b19e02604a9178760f0b1c8ff
Mahatthana Nomsawadi <mahatthana.n@gmail.com>
2019-06-01 11:39:43 +0700

    :sparkles: Register hitter to admin site

--- hit_me_please/hitters/tests.py
+++ hit_me_please/hitters/tests.py
@@ -1,8 +1,17 @@
+from django.contrib import admin
 from django.test import TestCase
 
+from .admin import HitterAdmin
 from .models import Hitter
 
 
 class HitterTest(TestCase):
     def test_email_field(self):
-        Hitter.objects.create(email='a@b.com')
+        email = 'a@b.com'
+        hitter = Hitter.objects.create(email=email)
+        self.assertEqual(hitter.email, email)
+
+
+class HitterAdminTest(TestCase):
+    def test_hitter_registration(self):
+        self.assertIsInstance(admin.site._registry[Hitter], HitterAdmin)
